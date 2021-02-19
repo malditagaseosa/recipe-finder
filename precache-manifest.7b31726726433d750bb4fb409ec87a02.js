392ebcf26b15c1e12ccdf847365bb923f949d0f3
Camilo Barrios Villamizar <malditagaseosa@gmail.com>
2021-02-18 19:43:28 -0500

    Updates

---
+++ precache-manifest.7b31726726433d750bb4fb409ec87a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0c0cee02e08129ebdb5b99848e6e8c",
+    "url": "/recipe-finder/index.html"
+  },
+  {
+    "revision": "96870e81117f14919c9e",
+    "url": "/recipe-finder/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e101e9069d76e95a85b9",
+    "url": "/recipe-finder/static/css/main.31302a80.chunk.css"
+  },
+  {
+    "revision": "96870e81117f14919c9e",
+    "url": "/recipe-finder/static/js/2.a083f18d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/recipe-finder/static/js/2.a083f18d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e101e9069d76e95a85b9",
+    "url": "/recipe-finder/static/js/main.1fc2cce8.chunk.js"
+  },
+  {
+    "revision": "8bae7f566c0a2aab6409",
+    "url": "/recipe-finder/static/js/runtime-main.6da655e3.js"
+  }
+]);
