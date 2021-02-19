d25ea58eee748a995256eb43d8551c55fe1c89d7
Camilo Barrios Villamizar <malditagaseosa@gmail.com>
2021-02-18 20:00:42 -0500

    Uso de HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import FavoriteRecipeList from './components/FavoriteRecipeList';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -13,12 +13,12 @@ store.subscribe(() => console.log('store: ', store.getState()));
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <Router>
             <Switch>
                 <Route exact path="/" component={ App } />
                 <Route path="/favorites" component={ FavoriteRecipeList } />
             </Switch>
-        </BrowserRouter>        
+        </Router>        
     </Provider>, 
     document.getElementById('root')
 );
