398f5da3ce2b0dcbcff4581a65a750a4763a22f3
Camilo Barrios Villamizar <malditagaseosa@gmail.com>
2021-02-18 19:29:10 -0500

    Componente para titulo

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <h1 className="mb-3">10 Recipes <span role="img" aria-label="emojis">👩‍🍳🍲🍕</span></h1>
+    );
+}
+
+export default Title;
